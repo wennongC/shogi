ec23bb4dcbb85ddc94ed20ae07a9f9efd444fb9f
Wennong <caiwennong@gmail.com>
2021-06-19 08:46:32 +0800

    Updated home page description

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -7,6 +7,18 @@ export default class HomePage extends React.Component {
                 <h1>Shogi Game 将棋ゲーム</h1>
                 <hr />
                 <a href="/game"><button>Start the Game</button></a>
+                <hr />
+                <h3>Game Control:</h3>
+                <p><strong>right click a figure</strong>: select a figure</p>
+                <p>(after above):</p>
+                <ul>
+                    <li><p><strong>right click it again </strong>: cancel the selection</p></li>
+                    <li><p><strong>right click a empty cell or a cell has enemy figure</strong>: move the figure</p></li>
+                </ul>
+                <p><strong>left click a figure</strong>: turn that figure into back side</p>
+                <hr />
+                <p>No programming rule to restrict how to move the figure</p>
+                <p>ルールとマナーを守って楽しくデュエルしよう！</p>
             </div>
         );
     }
